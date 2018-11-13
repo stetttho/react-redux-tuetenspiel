bd0b88ba2fcb743e7786ae4c1a08e1652877ea04
Thomas Stettler <stetttho@students.zhaw.ch>
2018-11-13 12:59:15 +0100

    Updates

---
+++ precache-manifest.ce27b1fa5362dfde92004ff7a5ad82a9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a3d2887acf81aa8927c",
+    "url": "/react-redux-tuetenspiel/static/js/runtime~main.7a3d2887.js"
+  },
+  {
+    "revision": "00c7302783f9aaca6bed",
+    "url": "/react-redux-tuetenspiel/static/js/main.00c73027.chunk.js"
+  },
+  {
+    "revision": "b1f568160b97a6b59c31",
+    "url": "/react-redux-tuetenspiel/static/js/1.b1f56816.chunk.js"
+  },
+  {
+    "revision": "e2713296b6d65246a3057952845ee4b2",
+    "url": "/react-redux-tuetenspiel/index.html"
+  }
+];
